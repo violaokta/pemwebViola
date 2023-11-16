423addd9c872480f2f7aa194e6edb772f6a192f5
violaokta <violaokta.rn@gmail.com>
2023-11-16 19:32:18 +0700

    upload tugas

---
+++ backend/delete.php
@@ -0,0 +1,18 @@
+<?php
+require '../config/db.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $delete_query = mysqli_query($db_connect, "DELETE FROM products WHERE id = $id");
+
+    if ($delete_query) {
+        header("Location: ../show.php"); 
+        exit();
+    } else {
+        echo "Gagal menghapus data.";
+    }
+} else {
+    echo "ID tidak valid.";
+}
+?>
